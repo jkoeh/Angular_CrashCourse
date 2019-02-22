0e1bccee248fd21a5ce33d09a1e1ac93a96ee61a
Koeh <johnkoeh@gmail.com>
2019-02-22 08:45:46 -0800

    course

--- helloworld/src/app/app.module.ts
+++ helloworld/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoursesComponent } from './courses.component';
+import { ComComponent } from './com/com.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CoursesComponent,
+    ComComponent
   ],
   imports: [
     BrowserModule,
